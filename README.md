0574d0f1428b092994ab8a2402d4fc5f2c0d6022
Jim Mahoney <mahoney@marlboro.edu>
2014-02-16 16:23:09 -0500

    lots more documentation

--- README.md
+++ README.md
@@ -1,12 +1,160 @@
 tron-engine
 ===========
 
-Infrastructure for a Tron robot tournament. Based on http://tron.aichallenge.org/ .
+Infrastructure for a Tron robot tournament, 
+based on the 2010 U Waterloo Google AI Challenge.
 
 See 
 
+* http://tron.aichallenge.org/
 * https://github.com/MarlboroCollegeComputerScience/tron-engine
 * http://cs.marlboro.edu/courses/spring2014/workshop/home
 * http://csmarlboro.org/tron/
 
+The game
+--------
+
+The game is based on the light cycle arena in the 1982 Tron movie,
+in which motorcyles zooming around in a room leave walls of light 
+behind themselves, trying to box in the other player.
+
+Here a referee progam (engines/round.py or engines/tournament.py) runs
+two robot programs, sending them ascii maps and receiving back the
+direction they choose to move, all through stdin and stdout.
+
+<img src="lib/example_game.png">
+
+There are a number of example bots and referees in various languages
+at tron.aichallenge.org.
+
+How do I use this?
+------------------
+
+You have several choices.
+
+The quick and dirty way to run a robot is to execute it
+from the command line, manually playing the role of 
+the referee. This is the simplest way to see if a robot
+will run at all.
+
+For example
+
+    $ ./bots/wallbot
+    5 4              < You type this,
+    #####            < giving a 5 columns x 4 rows map where 
+    #1 2#            <   '#'  is a wall
+    #   #            <   '1'  is you
+    #####            <   '2'  is your opponent,
+    3                > and the bot prints this (meaning SOUTH).
+
+See the "protocol" section below for all the details of the
+dialog between the referee and the bot.
+
+To see one game in a terminal, use the ./run script, which
+contains the syntax needed to start up a referee, 
+set the bots, and choose a room. (See the text files
+in the maps/ folder.) After editing ./run appropriately, 
+just type
+
+    $ ./run
+
+Depending on the referee's options and the bot code, the moves from a
+game can be saved, typically in the games/ folder, and diagnostics and
+errors may be written to a files, typically in the logs/ folder.
+
+The replay/ folder contains a web app that can replay saved games; visit
+http://csmarlboro.org/tron and 
+http://csmarlboro.org/tron/replay/tourney.cgi?name=test .
+
+The tournament/ folder contains folders for various tournaments,
+including scripts and results. See the files in tournament/test/ 
+for an example.
+
+The protocol
+------------
+
+On each turn, each bot gets a map (via its stdin) and must send back
+(via its stdout) a direction (1, 2, 3, 4), within about 1 second.
+
+If a bot produces anything else, or takes too long, it forfeits.
+
+If both bots move into a wall, or move into each other or into
+the same location, or both forfeit, then the game is a draw.
+
+If one bot makes a legal move but the other doesn't then it wins
+and the other loses.
+
+The various starter packages and library files in the bots/ folder
+(e.g. bots/tron.py, bots/c/tronlib.c) provide APIs for reading and
+analyzing the map, logging errors and diagonstics, and sending the
+move to the referee. See those file for the specifics - they're all a
+bit different depending on the language style and preferences of the
+authors.
+
+The format that the referee uses to send the map to the bot 
+is the same on each turn, and and is also the same as the 
+files in the maps/ folder. For example, maps/ring.txt looks like
+
+    15 15
+    ###############
+    #    2        #
+    # ##### ##### #
+    # #         # #
+    # #         # #
+    # #         # #
+    # #         # #
+    #             #
+    # #         # #
+    # #         # #
+    # #         # #
+    # #         # #
+    # ##### ##### #
+    #        1    #
+    ###############
+
+The first line gives the width and height of the map.
+Then each of the following (height) lines has in its
+first (width) characters the layout of the current room.
+
+The tiles of the grid are
+
+    '1'        'me', the current player
+    '2'        'them', the opponent
+    '#'        wall
+    ' '        floor
+
+The referee sends each player the board from their point
+of view. So for example at the same time that the player
+near the bottom of the map gets the board above, the
+other player near the top receives
+
+    15 15
+    ###############
+    #    1        #
+    # ##### ##### #
+    # #         # #
+    # #         # #
+    # #         # #
+    # #         # #
+    #             #
+    # #         # #
+    # #         # #
+    # #         # #
+    # #         # #
+    # ##### ##### #
+    #        2    #
+    ###############
+
+The entire map, including the "width height" dimensions,
+is read on stdin by each bot on every turn.
+
+About
+-----
+
+The Programming Workshop course at Marlboro College
+is using this as a group exercise.
+
+Questions? Ask Jim.
+
+Jim Mahoney | cs.marlboro.edu | MIT License | Feb 2014
 
--- bots/tron.py
+++ bots/tron.py
@@ -1,9 +1,12 @@
 """
 tron.py
 
- Tron bot utilities.
+Tron bot utilities  (Python 2.7.5)
 
- Usage:
+See ../README.md for the rules and conventions.
+
+Usage
+-----
 
    1. Create bot1.py, bot2,py files, i.e. :
 
@@ -15,14 +18,140 @@ tron.py
      for board in tron.Board.generate():
        tron.move(which_move(board))
 
+   There are a number of examples in the bots/ folder, e.g. wallbot.py,
+   randbot.py, logbot.py, etc.
+       
    2. Then run 'em against each other e.g.
 
      $ ./engines/round.py --FPS=5 -v -B maps/empty-room.txt bots/bot1.py bots/bot2.py
+    
+   See the "run" script for more examples of using the round.py referee.
 
- Based closely on provided code for the Python 2.5 starter package from the 
- Tron AI Challenge at http://csclub.uwaterloo.ca/contest/starter_packages.php .
+Map conventions
+---------------
 
- Jim Mahoney, Marlboro College | GPL | Feb 10 2010 ; modified Jan 2014
+The game grid is a rectangular matrix of tiles consisting of
+walls, floors, you, and your opponent. On each turn you're given
+a map, and you need analyze it to decide which direction to move.
+
+    tile         what's on a spot on the grid: one of (WALL, FLOOR, ME, THEM)
+    location     or coord, cell, or position: a pair of (row, column) integers
+    direction    one of (NORTH, EAST, SOUTH, WEST)
+    me           location of the current player (tile is ME which '1')
+    them         location of the opponent (tile is THEM which '2')
+
+Coordinates on the game grid start at (row=0, column=0)
+in the top left, i.e. (y=0, x=0), and go down to
+(row=width-1, column=height-1) at the bottom right,
+which is consistent with the way that matrices and
+image maps are typically labeled.
+
+   
+API
+---
+
+Each programming language has its own library of helper functions,
+which their own APIs.
+
+In this tron.py library, the objects and functions provided are
+documented below in the code - Your best bet is to look there for
+the specifics.
+
+Here's a brief summary of what might go in your mybot.py file
+
+    from tron import \
+      init_error_log, warn, \         # debugging
+      NORTH, SOUTH, EAST, WEST, \     # constants (1,2,3,4)
+      DIRECTIONS, \                   # list of directions
+      FLOOR, WALL, ME, THEM, \        # tiles (' ', '#', '1', '2')
+      direction, \                    # e.g. direction(NORTH) is 'north'
+      move, \                         # send a move to the referee
+      Board                           # read and analyze referee map
+
+      def which_move(board):
+          my_move = ...               # decide what your bot should do
+          return my_move
+      
+      for board in Board.generate():  # Get a board object for each game turn.
+          my_move = which_move(board)
+          move(my_move)               # send your move to the referee.
+                                          
+The board object gives you information about the current situation.
+
+       board.height, board.width        # integers
+       board.me()                       # my (row, column) location on the grid
+       board.them()                     # opponent (row, column) location
+       board.passable(location)         # is location=(y,x) a FLOOR?
+
+       board.rel(direction, origin=me)  # The position reached by moving
+                                        # in that direction from the origin.
+                                        # (The default origin is board.me().)
+                                        # (Poorly named, IMHO ... but
+                                        # changing an API is always akward.)
+
+       board.adjacent(location)         # four horizontal, vertical neighbors
+       board.moves()                    # list of passable directions
+
+       board.as_string()                # printable version of the map
+
+Debugging
+---------
+ 
+Getting the bugs out of your bot can be tricky, since (a) you can't just
+print diagonstics, (since the referee communicates with the bot by stdio
+and stout), and (b) the bot is being invoked by the referee, not you.
+
+The bots/logbot.py shows how to send error and diagnostics to a log file.
+The details are described below.
+
+If you're getting errors from the referee that you don't understand,
+here are some things to check.
+
+    * Can you invoke your bot from the command line by it's name?
+
+        $ ./mybot.py < input.txt
+
+      input.txt is a small sample of referee output, with two maps
+      followed by an illegal line (xx), which should make the bot stop.
+
+        $ ./wallbot.py < input.txt 
+        3
+        2
+        Invalid input: expected dimensions on first line      
+
+      If your bot doesn't do something like this, make sure it's executable
+      and has the correct invocation line (e.g. "#!/usr/bin/env python")
+      at the top.
+
+        $ chmod +x mybot.py
+
+    * Can you do that from another folder?
+
+        $ cd ..
+        $ ./bots/mybot.py < bots/input.txt
+
+      If not, your bot may be trying be failing to find a log file.
+
+    * Is your bot writing to a log file that the referee can't write to?
+      You can check the permissions with
+
+        $ ls -al
+
+      And change a logfile so that anyone can write to it with
+
+        $ chmod o+w logfile.txt
+
+Before you spend too long time banging your head against the wall,
+ask for help.
+        
+About
+-----
+
+This library is based on the python code in the Python 2.5
+starter package from the Tron AI Challenge at
+http://csclub.uwaterloo.ca/contest/starter_packages.php .
+
+Jim Mahoney, Marlboro College | MIT License | Feb 2014
 """
 import sys, os, datetime
 
@@ -109,20 +238,20 @@ THEM  = '2'
 
 DIRECTIONS = (NORTH, EAST, SOUTH, WEST)
 
-
 def direction(which):
     """ Return a string (e.g. 'north') from a direction number (e.g. 1) """
     return ['', 'north', 'east', 'south', 'west'][which]
 
 def move(direction):
+    """ Send a move to the referee. """
     print direction
     sys.stdout.flush()
 
-def invalid_input(message):
+def _invalid_input(message):
     print >>sys.stderr, "Invalid input: %s" % message
     sys.exit(1)
 
-def readline(buf):
+def _readline(buf):
     while not '\n' in buf:
         tmp = os.read(0, 1024)  # standard input, max 1kB
         if not tmp:
@@ -131,7 +260,7 @@ def readline(buf):
     if not buf.strip():
         return None, buf
     if not '\n' in buf:
-        invalid_input('unexpected EOF after "%s"' % buf)
+        _invalid_input('unexpected EOF after "%s"' % buf)
     index = buf.find('\n')
     line = buf[0:index]
     rest = buf[index + 1:]
@@ -148,26 +277,26 @@ class Board(object):
         self._them = None
 
     @staticmethod
-    def read(buf):
-        meta, buf = readline(buf)
+    def _read(buf):
+        meta, buf = _readline(buf)
         if not meta:
             return None, buf
         dim = meta.split(' ')
         if len(dim) != 2:
-            invalid_input("expected dimensions on first line")
+            _invalid_input("expected dimensions on first line")
         try:
             width, height = int(dim[0]), int(dim[1])
         except ValueError:
-            invalid_input("malformed dimensions on first line")
+            _invalid_input("malformed dimensions on first line")
         lines = []
         while len(lines) != height:
-            line, buf = readline(buf)
+            line, buf = _readline(buf)
             if not line:
-                invalid_input("unexpected EOF reading board")
+                _invalid_input("unexpected EOF reading board")
             lines.append(line)
         board = [line[:width] for line in lines]
         if len(board) != height or any(len(board[y]) != width for y in xrange(height)):
-            invalid_input("malformed board")
+            _invalid_input("malformed board")
         return Board(width, height, board), buf
 
     @staticmethod
@@ -175,12 +304,12 @@ class Board(object):
         """ Generate board objects, once per turn. """
         buf = ''
         while True:
-            board, buf = Board.read(buf)
+            board, buf = Board._read(buf)
             if not board:
                 break
             yield board
         if buf.strip():
-            invalid_input("garbage after last board: %s" % buf)
+            _invalid_input("garbage after last board: %s" % buf)
 
     def __getitem__(self, coords):
         """ Retrieve the object at the specified coordinates.
@@ -209,7 +338,7 @@ class Board(object):
             It is always true that board[board.me()] == tron.ME.
             """
         if not self._me:
-            self._me = self.find(ME)
+            self._me = self._find(ME)
         return self._me
 
     def them(self):
@@ -217,10 +346,10 @@ class Board(object):
             It is always true that board[board.them()] == tron.THEM.
             """
         if not self._them:
-            self._them = self.find(THEM)
+            self._them = self._find(THEM)
         return self._them
 
-    def find(self, obj):
+    def _find(self, obj):
         for y in xrange(self.height):
             for x in xrange(self.width):
                 if self[y, x] == obj:
@@ -256,7 +385,7 @@ class Board(object):
             raise KeyError("not a valid direction: %s" % direction)
 
     def adjacent(self, origin):
-        """ Calculate the four tiles that are adjacent to origin.
+        """ Calculate the four locations that are adjacent to origin.
             Particularly, board.adjacent(board.me()) returns the four
             tiles to which you can move to this turn. This does not
             return tiles diagonally adjacent to origin.
